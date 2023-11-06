c533379d31c3715439bf07623b51f467b1b57321
razvan0710 <125794861+razvan0710@users.noreply.github.com>
2023-11-06 19:07:28 +0200

    first one

---
+++ src/test/java/com/in20min/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in20min.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
